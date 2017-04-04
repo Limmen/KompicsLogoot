bc29c8da06bf432c70bff32c7f5bc2a55d836c96
Limmen <kimham@kth.se>
2017-04-04 16:24:40 +0200

    remove simlogs

---
+++ src/test/java/se/kth/tests/beb_test/BEBTest.java
@@ -0,0 +1,14 @@
+package se.kth.tests.beb_test;
+
+import org.junit.Test;
+
+/**
+ * @author Kim Hammar on 2017-04-04.
+ */
+public class BEBTest {
+
+    @Test
+    public void bestEffortDeliveryTest(){
+
+    }
+}
